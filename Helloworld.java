2d885d0e193948df597d05b005f0e82274f3415f
matthew.yip <matthewyip14@gmail.com>
2025-04-10 22:00:42 +0800

    learn System.out.println()

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    // I Love Java
+    // 你好👋
+
+    // System.out.println is a tool
+    System.out.println("goodbye");
+  }
+}
