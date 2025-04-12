a61cd74a0f13c6974b4376a563bc3506eb8cb857
matthew.yip <matthewyip14@gmail.com>
2025-04-12 16:05:29 +0800

    homework_v2

--- homework/helloworld.java
+++ homework/helloworld.java
@@ -1,7 +1,9 @@
 package homework;
 
 public class helloworld {
-  public static void main(String[] args)
-   System.out.println("Hello... I'm Matthew")
+
+  public static void main(String[] args){
+    System.out.println("Hello! I'm Matthew");
+  }
 
 }
