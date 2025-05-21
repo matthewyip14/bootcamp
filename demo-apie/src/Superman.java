1303ed8fc6c251ca57fbd540c37b93687e802b32
matthew.yip <matthewyip14@gmail.com>
2025-05-22 02:15:39 +0800

    20250522v1

--- demo-apie/src/Superman.java
+++ demo-apie/src/Superman.java
@@ -3,12 +3,12 @@ public class Superman extends Human implements Flyable, Living {
   public Superman(String name) {
     super(name);
   }
-  
+
   @Override
   public void fly() {
     System.out.println("I am flying ...");
   }
-  
+
   @Override
   public void eat() {
     System.out.println("I am eating ...");
@@ -21,30 +21,35 @@ public class Superman extends Human implements Flyable, Living {
 
   @Override
   public void walk() {
-    System.out.println("Superman is walking ...");
+    System.out.println("Superman is walking ....");
   }
 
   // ! You can override or NOT override speak()
   @Override
   public void speak() {
-    System.out.println("Superman is speaking ...");
+    System.out.println("Superman is speaking ....");
   }
 
   public static void main(String[] args) {
     Superman s1 = new Superman("John");
     s1.fly();
     s1.eat();
+    s1.walk();
+    s1.speak();
+    s1.drink();
 
     Flyable something = new Superman("Peter");
     something.fly(); // Polymorphism
-  
-
-    // hide the method of eat()
+    
+    // Hide the method of eat() and drink()
     // something.eat();
     // something.drink();
 
     Superman s2 = (Superman) something;
     s2.eat();
+    s2.walk();
+    s2.drink();
+    s2.speak();
 
     Living s3 = new Superman("Steven");
     s3.eat();
